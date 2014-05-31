5d8a36f8ffc29f196f307d8beda68db3ddaf156d
Wayne Sanderson <w.sanderson@utah.edu>
2014-05-31 19:35:40 +0000

    Add config.ru

--- site.rb
+++ site.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
-set :port, 4000
-set :bind, '0.0.0.0'
-get '/' do
-  erb :index
+
+  ENV['PORT'] ||= '4000'
+  set :port, ENV['PORT']
+  set :bind, '0.0.0.0'
+
+  get '/' do
+    erb :index
 end
 
 get '/italian' do
